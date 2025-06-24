d89546b8970c2d7ec9d19a7b6787fde84d4a5e9e
Soham Sattigeri <sattigeri.soham@gmail.com>
2025-06-24 10:48:43 +0530

    Update global styles for dark theme and enhance SVG background opacity
    - Adjusted color variables in the dark theme for improved contrast and readability.
    - Increased opacity of SVG backgrounds to enhance visual depth and clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const ArrowIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 25 25"
     style={{ width: "16px", height: "16px" }}
-    className="w-4 h-4"
+    className="w-4 h-4 fill-foreground"
   >
     <path
       d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z"
